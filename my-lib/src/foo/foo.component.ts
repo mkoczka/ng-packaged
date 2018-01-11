cc22f067c733b2d22d98d4b06f56b2b8a4670742
koczkam <miroslav.koczka@softec.sk>
2018-01-11 13:47:25 +0100

    test preserveWhitespaces option

--- my-lib/src/foo/foo.component.ts
+++ my-lib/src/foo/foo.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-foo',
-  template: '<h1>foo</h1>',
-  styleUrls: ['./foo.component.scss']
+  template: '<h1> foo </h1>',
+  styleUrls: ['./foo.component.scss'],
+  preserveWhitespaces: false
 })
 export class FooComponent {
 }
